26726159a51c1c250ba657e7b289b9280d9bde81
Pablo Canale <pablocanale.exa@gmail.com>
2021-11-09 09:39:36 -0300

    Created folders and files initial structure of my web

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,9 @@
+import "./intro.scss"
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            Aca va la intro vithe
+        </div>
+    )
+}
---
+++ src/components/testimonials/Testimonial.jsx
@@ -0,0 +1,9 @@
+import "./testimonials.scss"
+
+export default function Testimonial() {
+    return (
+        <div className="testimonials">
+            This are testimonials
+        </div>
+    )
+}
